6d0d7b0d3b4c68c1f32c221a5299c5310d444854
minj0206 <alssl03@naver.com>
2025-04-08 16:35:00 +0900

    2주차 과제

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    context = {
+        'generation': 13,
+        'info': {'weather': '좋음', 'feeling': '배고픔(?)', 'note': '아기사자화이팅!'},
+        'shortKeys': [
+            '들여쓰기: Tab',
+            '내어쓰기: Shift + Tab',
+            '주석처리: 윈도우-Ctrl + /, 맥-command + /',
+            '자동정렬: Shift + Alt + F or Ctrl + K + F',
+            '한줄이동: Alt + 방향키(위/아래)',
+            '한줄삭제: Ctrl + Shift + k or Ctrl + x',
+            '같은단어전체선택: Ctrl + Shift + L'
+        ]
+}
+    return render(request, 'main/mainpage.html', context)
+def secondpage(request):
+    return render(request, 'main/secondpage.html')
